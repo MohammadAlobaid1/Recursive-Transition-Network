5f8529bd9bcb1597327a5844e797e5d0b5339cb2
Thomas Graf <git@thomasgraf.net>
2020-04-12 12:25:49 -0400

    initial commit

---
+++ 00_rtn/rtn_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class RTN:
+    def __init__(self,
+                 subautomata,
+                 initial):
+        pass
+
+    def is_consistent(self):
+        pass
+
+    def missing_subautomata(self):
+        pass
+
+    def accepts(self, string):
+        pass
+
+    def trace(self, string):
+        pass
+
+    def to_fsa(self, bound):
+        pass
